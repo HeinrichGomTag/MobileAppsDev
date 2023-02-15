cd9feea1c698e89aab09f8c68a350d09721792f4
eUlisesBaezGt <0241823@up.edu.mx>
2023-02-14 19:44:05 -0600

    App1 Changed new User

---
+++ App1/App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Enrique Gomez Tagle on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
