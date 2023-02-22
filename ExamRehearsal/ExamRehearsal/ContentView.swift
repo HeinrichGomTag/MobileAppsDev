72dde39857cf3c58018a3546cb5191b933e0259e
eUlisesBaezGt <0241823@up.edu.mx>
2023-02-21 18:49:05 -0600

    Antes del repaso

---
+++ ExamRehearsal/ExamRehearsal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExamRehearsal
+//
+//  Created by Enrique Gomez Tagle on 21/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExamRehearsal/ExamRehearsal/ExamRehearsalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamRehearsalApp.swift
+//  ExamRehearsal
+//
+//  Created by Enrique Gomez Tagle on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamRehearsalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
