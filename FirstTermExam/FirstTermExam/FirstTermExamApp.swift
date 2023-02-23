f2d3802f436557c10fc5fee59edeb81914240cf5
eUlisesBaezGt <0241823@up.edu.mx>
2023-02-23 12:07:30 -0600

    Intento falla examen

---
+++ FirstTermExam/FirstTermExam/FirstTermExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTermExamApp.swift
+//  FirstTermExam
+//
+//  Created by Enrique Gomez Tagle on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstTermExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
