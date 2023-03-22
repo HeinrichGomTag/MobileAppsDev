600fd3a39b1158fa6c4e32bd89b592aecc0d04b9
eUlisesBaezGt <0241823@up.edu.mx>
2023-03-21 19:51:25 -0600

    Casi acabo el de las frutas

---
+++ navigation-fruit1/navigation-fruit1/navigation_fruit1App.swift
@@ -0,0 +1,17 @@
+//
+//  navigation_fruit1App.swift
+//  navigation-fruit1
+//
+//  Created by Enrique Gomez Tagle on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct navigation_fruit1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
