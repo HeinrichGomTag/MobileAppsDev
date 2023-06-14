4910fc9906c583b8825ad94c751fcc2d552e93eb
HeinrichGomTag <0241823@up.edu.mx>
2023-06-14 11:51:33 -0600

    types
    Co-Authored-By: KikinAcademy <103606817+kikinacademy@users.noreply.github.com>

---
+++ Certification/types.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+import UIKit
+
+// Types
+
+var x = 3 // Integer
+var name = "Joe" // string
+
+type(of: name)
+
+var lastName: String = "123"
+
+let age: Int = 22
+let price: Double = 1.99
+var heigh: Float = 20.1231231231
+
+class Person{
+    
+}
+
+var John: Person = Person()
+
+var button1: UIButton = UIButton()
+
+var isComplete: Bool = false
+isComplete = true
+
+
--- Certification/vars.playground/Contents.swift
+++ Certification/vars.playground/Contents.swift
@@ -11,11 +11,23 @@ var y = 1
 
 y = 2
 
-var z = 3
-z = 12
+let z = 3
+// z = 12
 
 class Person{
-    static let dave = Person()
+    static let age = 12
+    let lastName = "Smith"
 }
 
 var John = Person()
+var Jen = Person()
+var Bri = Person()
+var Tim = Person()
+
+let dave = Person.age
+
+let price = 1.23
+var name = "Joe"
+name = "Jill"
+
+122
