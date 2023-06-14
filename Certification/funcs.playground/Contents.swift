4d6d18a6e9879d664a8418f4629457dfa1f3a188
HeinrichGomTag <0241823@up.edu.mx>
2023-06-14 12:05:15 -0600

    funcs & Params
    Co-Authored-By: KikinAcademy <103606817+kikinacademy@users.noreply.github.com>

--- Certification/funcs.playground/Contents.swift
+++ Certification/funcs.playground/Contents.swift
@@ -1,3 +1,46 @@
+
 import Foundation
 
 // Funcs and Params
+
+func myFirstFunction(){
+    
+}
+
+func addTwoNumbers1(a: Int, b:Int){
+    print(a+b)
+}
+
+func addTwoNumbers2(a: Int, b:Int) -> Int{
+    return a+b
+}
+
+func SquareNumber(number: Int) -> Int{
+    return number * number
+}
+
+func log(){
+    
+}
+
+addTwoNumbers1(a: 1, b: 2)
+
+var x = 3
+var y = 1232132
+let sum = addTwoNumbers2(a: x, b: y)
+
+let square = SquareNumber(number: x)
+print(square)
+
+log()
+
+func isEven(num: Int) -> Bool {
+    if (num % 2 == 0){
+        return true
+    }
+    return false
+}
+
+print(isEven(num: 5))
+print(isEven(num: 8))
+print(isEven(num: 720))
