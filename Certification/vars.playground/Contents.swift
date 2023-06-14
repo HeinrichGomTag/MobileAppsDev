5ba788e38150c787dfcbac25010c3d727bf8b271
HeinrichGomTag <0241823@up.edu.mx>
2023-06-14 11:22:37 -0600

    Swift Variables

---
+++ Certification/vars.playground/Contents.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+// Variable, Constant, Static
+
+import Foundation
+
+var str = "Hello World"
+
+var x = 4
+var y = 1
+
+y = 2
+
+var z = 3
+z = 12
+
+class Person{
+    static let dave = Person()
+}
+
+var John = Person()
