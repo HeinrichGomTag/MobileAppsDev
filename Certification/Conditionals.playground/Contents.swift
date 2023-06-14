b327c773e82e905e38d53b6e089d19674474989c
HeinrichGomTag <0241823@up.edu.mx>
2023-06-14 17:55:31 -0600

    conditionals
    Co-Authored-By: KikinAcademy <103606817+kikinacademy@users.noreply.github.com>

---
+++ Certification/Conditionals.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+// Conditionals
+
+var x = 19
+
+if x > 20 {
+
+} else if x > 10{
+    
+} else{
+    // Something else
+}
+var y: Int? = nil
+
+if let realNumber = y {
+    // Use value in here
+}
+
+var z = 1
+var w = 12
+
+if x % w == 0{
+    print("Even")
+}
+
+if x > 10 && x < 200 {
+}
+
+if x > 10 || x < 200 {
+}
