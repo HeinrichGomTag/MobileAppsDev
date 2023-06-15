fc1ec5b1ca12bc8bb062885a9fdd0172552f35ff
HeinrichGomTag <0241823@up.edu.mx>
2023-06-15 11:43:31 -0600

    Naming Conventions
    Co-Authored-By: KikinAcademy <103606817+kikinacademy@users.noreply.github.com>

---
+++ Certification/NamingConventions.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+var number_of_bank_accounts = "" // SNAKE
+var numberOfBankAccounts = "" // CAMEL
+var NumberOfBankAccounts = "" // PASCAL
+
+func is_larger_than_five() {}
+func isLargerThanFive() {}
+
+class BigPerson {}
+
+var joe = BigPerson()
