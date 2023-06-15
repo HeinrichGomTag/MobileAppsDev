467a5766413f7a53890abe85e14c1b3508abfcca
HeinrichGomTag <0241823@up.edu.mx>
2023-06-15 12:24:15 -0600

    math
    Co-Authored-By: KikinAcademy <103606817+kikinacademy@users.noreply.github.com>

--- Certification/math.playground/Contents.swift
+++ Certification/math.playground/Contents.swift
@@ -1,3 +1,31 @@
-import UIKit
 
-var greeting = "Hello, playground"
+import Foundation
+
+var x = 3
+var y = 2
+
+var result = x + y
+var result2 = x - y
+var result3 = x / y
+var result4 = x * y
+
+x = 30
+y = 7
+
+result = x % y
+
+if result == 0 {
+    
+} else {
+    
+}
+
+func add (x: Int, y: Int) -> Int {
+    var result = x + y
+    while (result < 10) {
+        result = add(x: result, y: 1)
+    }
+    return result
+}
+
+add(x: 1,y: 2)
