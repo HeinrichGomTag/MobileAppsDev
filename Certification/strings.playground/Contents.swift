8a636a79b299f8503b0a76c80902000b7d6a6c8c
HeinrichGomTag <0241823@up.edu.mx>
2023-06-15 11:09:23 -0600

    strings
    Co-Authored-By: KikinAcademy <103606817+kikinacademy@users.noreply.github.com>

---
+++ Certification/strings.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+
+// Strings
+
+var first = "Steve"
+var last = "Jobs"
+var number = 1
+
+let fullname = "My name is -> \(first) \(last) and I'm am number \(number)"
+print(fullname)
+
+
+let lenght = fullname.count
+
