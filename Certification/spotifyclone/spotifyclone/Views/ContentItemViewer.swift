f6358d053d402a07d4a077e10b4b818297410b68
HeinrichGomTag <0241823@up.edu.mx>
2023-06-15 21:18:35 -0600

    FINISHED: Check Color Tab bar

--- Certification/spotifyclone/spotifyclone/Views/ContentItemViewer.swift
+++ Certification/spotifyclone/spotifyclone/Views/ContentItemViewer.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ContentItemViewer: View {
-    var topSpacer_height: CGFloat = 400
+    var topSpacer_height: CGFloat = 350
+    var reusableColor = Color.init(red: 61/255, green: 89/255, blue: 115/255)
+    @State var playButton_offset: CGFloat = 335
     
     var body: some View {
         ZStack {
             // LAYER 0
-            LinearGradient(gradient: Gradient(colors: [Color.init(red: 61/255, green: 89/255, blue: 115/255), Color.black.opacity(0.94), Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [reusableColor, Color.black, Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
             // LAYER 1, ALBUM DETAILS
             VStack {
                 Spacer()
-                    .frame(height: 50)
+                    .frame(height: 20)
                 
                 Image("cover")
                     .resizable()
@@ -34,17 +36,21 @@ struct ContentItemViewer: View {
                     .padding(.top, 1)
                     .frame(alignment: .trailing)
                 
-                Text("Sencillo • 2022")
-                    .multilineTextAlignment(.leading)
-                    .foregroundColor(.init(red: 0.5, green: 0.5, blue: 0.5))
-                    .font(.system(size: 16))
-                    .padding(.top, 1)
-                
                 Spacer()
             }
             
             // LAYER 2, ITEM LIST
             ScrollView {
+                GeometryReader{geo -> AnyView? in
+                    let thisOffset = geo.frame(in: .global).minY
+                    if thisOffset > -300 {
+                        self.playButton_offset = thisOffset
+                    } else {
+                        self.playButton_offset = -300
+                    }
+                    
+                    return nil
+                }
                 VStack(spacing: 0) {
                     HStack {
                         Spacer()
@@ -52,7 +58,8 @@ struct ContentItemViewer: View {
                             .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.clear, Color.clear, Color.clear, Color.black]), startPoint: .top, endPoint: .bottom))
                     }
                     VStack {
-                        ForEach(0..<10){ element in
+                        ForEach(0..<30
+                        ){ element in
                             HStack{
                                 lImage_text_rlImage()
                                 Spacer()
@@ -62,12 +69,64 @@ struct ContentItemViewer: View {
                 }.background(Color.clear)
             }
             
+            // Background Layer for layer 4 / layer 3
+            VStack{
+                LinearGradient(gradient: Gradient(colors: [reusableColor, Color.clear]), startPoint: .top, endPoint: .bottom).frame(height: 0)
+                Spacer()
+            }.edgesIgnoringSafeArea(.all)
+            
             // LAYER 3
-            Text("Play")
+            VStack {
+                Spacer()
+                    .frame(height: playButton_offset + 250)
+                
+                HStack{
+                    if (playButton_offset > -300) {
+                        Text("PLAY")
+                    } else {
+                        Image(systemName: "play.fill")
+                    }
+                }
                 .foregroundColor(.white)
+                .frame(width: get_playButtonWidth(), height: 50)
                 .background(Color.init(red: 30/255, green: 215/255, blue: 96/255))
-                .frame(width: 2)
+                .cornerRadius(25)
+                .font(.system(size: 17, weight: .bold))
+                .shadow(radius: 20)
+                
+                Spacer()
+            }
+            
+            // Layer 4
+            VStack{
+                HStack {
+                    Image(systemName: "chevron.left")
+                    Spacer()
+                    Image(systemName: "ellipsis")
+                }.foregroundColor(.white)
+                    .padding()
+                Spacer()
+            }
+            
+            // Observer Layer
+            VStack{
+//                Text("\(playButton_offset)")
+//                    .foregroundColor(.yellow)
+                Spacer()
+            }
+            
         }
+    }
+        func get_playButtonWidth() -> CGFloat {
+            
+            if playButton_offset > -150 {
+                return 240
+            } else if playButton_offset <= -300 {
+                return 50
+            } else {
+                let width: CGFloat = 240 - (190*(((-1*playButton_offset) - 150) / 150))
+                return width
+            }
     }
 }
 
--- Certification/spotifyclone/spotifyclone/Views/ContentView.swift
+++ Certification/spotifyclone/spotifyclone/Views/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $selection){
-            HomePage()
+            ContentItemViewer()
                 .font(.title)
                 .tabItem{
                     VStack {
@@ -34,7 +34,7 @@ struct ContentView: View {
                     }
                 }
                 .tag(0)
-        }
+        }.toolbarBackground(.red, for: .tabBar)
     }
 }
 
